f4372fdcd7d09ea8a12dcab95aed97d792b2aa0d
Abhishek <abhimore74@gmail.com>
2023-02-26 17:52:18 -0600

    init

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const port = 3000
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'dbuser',
+//   password: 's3kreee7',
+//   database: 'my_db'
+// })
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
