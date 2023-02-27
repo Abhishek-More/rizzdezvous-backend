095e9f804c26d47773d57ffe0d5f5b2a0bc5ed4c
Abhishek <abhimore74@gmail.com>
2023-02-26 22:01:31 -0600

    lot of auth stuff

--- index.js
+++ index.js
@@ -1,30 +1,80 @@
+require('dotenv').config()
+
 const express = require('express')
 const mysql = require('mysql')
 const app = express()
-const port = 3000
+const port = 5500
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'dbuser',
-//   password: 's3kreee7',
-//   database: 'my_db'
-// })
+app.use(express.json());
 
-// connection.connect()
-
-// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-//   if (err) throw err
-
-//   console.log('The solution is: ', rows[0].solution)
-// })
-
-// connection.end()
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME 
 })
 
-app.listen(process.env.PORT || 3000, () => {
+connection.connect()
+
+app.get('/', (req, res) => {
+  res.send('Successful Connection')
+}) 
+
+app.get('/auth/authenticate', function(request, response) {
+	let email = request.body.email;
+	let password = request.body.password;
+	if (email && password) {
+		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results) {
+      if(error) {
+        throw error
+      }
+
+      if(results.length > 0) {
+        //send success code
+        response.status(200).send({
+          username: results[0].username,
+          email: results[0].email,
+          isManager: results[0].isManager
+        });
+      } else {
+        //send forbidden error code
+        response.status(403).send("Not Authenticated!");
+      }
+		});
+	} else {
+    response.status(400).send('Not all fields are filled in!');
+	}
+});
+
+app.post('/auth/create-user', function(request, response) {
+	let email = request.body.email;
+	let password = request.body.password;
+	let username = request.body.username;
+  let isManager = request.body.isManager;
+	if (email && password && username && isManager) {
+		connection.query('SELECT * FROM users WHERE email = ? OR username = ?', [email, username], function(error, results) {
+      if(error) {
+        throw error
+      }
+
+      if(results.length > 0) {
+        response.status(400).send("User already exists");
+      } else {
+        connection.query('INSERT INTO users (username, email, password, isManager) VALUES (?, ?, ?, ?)', [username, email, password, isManager], function(error, results, fields) {
+          if(error) {
+            throw error
+          }
+          response.sendStatus(200).send("User created successfully");
+        });
+      }
+		});
+	} else {
+    response.sendStatus(400).send('Not all fields are filled in!');
+	}
+});
+
+
+app.listen(process.env.PORT || 5500, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
---
+++ init.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME 
+})
+
+function initUserTable(connection) {
+  console.log("Creating User Table...");
+  const initUserTableCommand = " \
+  CREATE TABLE IF NOT EXISTS `users` ( \
+    `id` int(11) NOT NULL AUTO_INCREMENT, \
+    `username` varchar(50) NOT NULL, \
+    `password` varchar(255) NOT NULL, \
+    `email` varchar(100) NOT NULL, \
+    `isManager` BOOLEAN NOT NULL, \
+    `isAdmin` BOOLEAN NULL, \
+    PRIMARY KEY (`id`) \
+  ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8; \
+  "
+  
+  connection.query(initUserTableCommand, (err, rows, fields) => {
+    if (err) throw err
+    console.log("Created User Table Successfully");
+  })
+}
+
+connection.connect();
+
+initUserTable(connection);
+//Create more init functions and put them here
+
+connection.end();
